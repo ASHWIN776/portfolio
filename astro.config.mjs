35bc47a799fc82e552c7e2e586ce0f32408eecf5
Ashwin Anil <ash@actu-al.co>
2026-02-03 23:50:40 +0900

    feat: added sitemap and robots.txt for the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   site: "https://ashwin.im",
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
